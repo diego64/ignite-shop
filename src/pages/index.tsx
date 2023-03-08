03f5e2142893de63e7e890f0c15145b5b5501049
diego64 <diegoferreira1964@gmail.com>
2023-03-08 12:04:43 -0300

    Configuracao de navegacao por link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import { GetServerSideProps, GetStaticProps } from "next";
+import Link from "next/link";
+import { GetStaticProps } from "next";
 import Stripe from "stripe";
 import { stripe } from "../lib/stripe";
 
@@ -30,14 +31,18 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-    
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link key={product.id} href={""}>
+            <Link href={`/product/${product.id}`} >
+              <Product className="keen-slider__slide">
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+        
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          </Link>
         )
       })
       }
@@ -60,7 +65,7 @@ export const getStaticProps: GetStaticProps = async () => {
       price: new Intl.NumberFormat('pt-BR', {
         style: 'currency',
         currency: 'BRL',
-      }).format(price.unit_amount / 100)
+      }).format(price.unit_amount as number/ 100)
     }
   })
 
