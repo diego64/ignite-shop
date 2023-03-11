399980a151757f736aff2db5a4d36da1d55dd2db
diego64 <diegoferreira1964@gmail.com>
2023-03-11 14:10:31 -0300

    Finalizacao da aplicacao

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,11 +1,11 @@
-import { stripe } from "@/src/lib/stripe";
-import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/product";
-import axios from "axios";
-import { GetStaticPaths, GetStaticProps } from "next";
+import Head from 'next/head';
 import Image from "next/image";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import { stripe } from "@/src/lib/stripe";
 import Stripe from "stripe";
+import axios from "axios";
+import { useState } from "react";
+import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/product";
+import { GetStaticPaths, GetStaticProps } from "next";
 
 interface ProductProps {
   product: {
@@ -40,21 +40,27 @@ export default function Product({ product }: ProductProps) {
   }
 
   return (
-    <ProductContainer>
-      <ImageContainer>
-        <Image src={product.imageUrl} width={520} height={480} alt=""/>
+    <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
+
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt=""/>
       </ImageContainer>
 
-      <ProductDetails>
-        <h1>{product.name}</h1>
-        <span>{product.price}</span>
-        <p>{product.description}</p>
-      
-        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
-          Comprar agora
-        </button>
-      </ProductDetails>
-    </ProductContainer>
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
+          <p>{product.description}</p>
+        
+          <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
   )
 }
 
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import Stripe from "stripe";
@@ -15,26 +16,43 @@ interface SuccessProps {
 }
 
 export default function Success({ customerName, product }: SuccessProps) {
-  return (
-    <SuccessContainer>
-      <h1>Compra Efetudada!</h1>
+    return (
+      <>
+        <Head>
+          <title>Compra Efetuada | Ignite Shop</title>
 
-      <ImageContainer>
-        <Image src={product.imageUrl} width={120} height={110} alt=""/>
-      </ImageContainer>
+          <meta name="robots" content="noindex" />
+        </Head>
 
-      <p>
-        Uhuul <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
-      </p>
+        <SuccessContainer>
+          <h1>Compra Efetudada!</h1>
 
-      <Link href="/">
-        Voltar ao catálogo
-      </Link>
-    </SuccessContainer>
+          <ImageContainer>
+            <Image src={product.imageUrl} width={120} height={110} alt=""/>
+          </ImageContainer>
+
+          <p>
+            Uhuul <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
+          </p>
+
+          <Link href="/">
+            Voltar ao catálogo
+          </Link>
+        </SuccessContainer>
+    </>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if(!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+  
   const sessionId = String(query.session_id);
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
